c24f69064f4d2ef024ed225912124eadb50a8540
Bayu Darmawan <darmawanbayu1@gmail.com>
2021-06-08 10:09:20 +0700

    update form register

--- WEB/js/home.js
+++ WEB/js/home.js
@@ -1,3 +1,11 @@
 function sign_up_request(){
     $("#loginModal").modal("hide");
 }
+
+const addAddress=()=>{
+    console.log('jalan')
+    $("#iframeAddress").removeClass('testingjquery')
+    $("iframeAddress").addClass('testingjquery2')
+    
+    console.log(a)
+}
