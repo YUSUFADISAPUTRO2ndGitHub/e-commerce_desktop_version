3e823e1a41a9fb6df6b6097008a0dd6022361074
Bayu Darmawan <darmawanbayu1@gmail.com>
2021-06-13 12:41:40 +0700

    update search item

--- WEB/js/jquery.js
+++ WEB/js/jquery.js
@@ -47,43 +47,53 @@ $(function(){
         
     });
 
-    $('.input-name').on('keyup',function(){
-        var value = $(this).val()
-        console.log(value)
-        
-        function sleep(ms) {
-            return new Promise(resolve => setTimeout(resolve, ms));
-          }
-          sleep(2000).then(()=>{
-              if(value.length>0){
-                  $('.box-render-search').css('display','block')
-                  $('.input-name').css('border-bottom-left-radius','0px')
-                  $('.input-name').css('border-bottom-right-radius','0px')
-                  console.log(allData)
-                  const searchString = value.toLowerCase()
-                      const filterSearch = allData.filter((item)=>{
-                          return (
-                              item.Name.toLowerCase().includes(searchString) 
-                          );
-                      });
-                      filterSearch.map((val,index)=>{
-                          $('.box-render-search').append(
-                              `
-                              <li>${val.Name}</li>
-                          `)
-                      })
-                      
-                      console.log(filterSearch)
-                  
-              }else {
-                  $('.box-render-search').css('display','none')
-                  $('.input-name').css('border-bottom-left-radius','25px')
-                  $('.input-name').css('border-bottom-right-radius','25px')
-              }
 
-          })
-    })
+    // var searchItem = []
+        $('.input-name').on('keyup', _.debounce(function () {
+            console.log('hi');
+            var value = $(this).val()
+            if(value.length>2){
+                console.log('masuk ke if')
+                $('.box-render-search').css('display','block')
+                $('.input-name').css('border-bottom-left-radius','0px')
+                $('.input-name').css('border-bottom-right-radius','0px')
+                $('.render-li-search').empty()
+            //   console.log(allData)
+                const searchString = value.toLowerCase()
+                console.log(searchString)
+                    const filterSearch = allData.filter((item)=>{
+                        return (
+                            item.Name.toLowerCase().includes(searchString) 
+                        );
+                    });
+                    // filterSearch.map((val,index)=>{
+                    //     $('.render-li-search').append(
+                    //         `
+                    //         <li>${val.Name}</li>
+                    //     `)
+                    // })
+                    // searchItem = filterSearch
+                    filterSearch.map((val,index)=>{
+                        $('.render-li-search').append(
+                            `
+                                <li>${val.Name}</li>
+                            `
+                        )
+                    })
+                    
+                    console.log(filterSearch)
+                
+            }else {
+                // searchItem = null
+            console.log('masuk ke if')
+                $('.box-render-search').css('display','none')
+                $('.input-name').css('border-bottom-left-radius','25px')
+                $('.input-name').css('border-bottom-right-radius','25px')
+            }
 
+        }, 2000));
+      
+    // });
 
     
    
